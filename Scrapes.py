1eea559f024f518d32c377315f1329f981f50222
WiseCthaeh <88669132+WiseCthaeh@users.noreply.github.com>
2021-12-03 12:56:41 +0100

    Update Scrapes.py

--- Scrapes.py
+++ Scrapes.py
@@ -4,23 +4,20 @@ from six.moves import urllib
 import json
 import pandas as pd
 
+key = 'your_API_key'
+dest = r'file_directory'
+
 def getUserTransaction(coin = 'btc', op = 'transactions', size = '1000'):   
     # expect string inputs. View Coinmetrics API documentation for more.
     # make sure key works   
     
     # get records with given uid and txnType
     url = 'https://api.coinmetrics.io/v4/blockchain/' + coin \
-        + '/' + op + '?api_key=I9e8A25wKUtV6fNnhkpk&page_size=' + size
+        + '/' + op + key + '&page_size=' + size
         
     # get data
     response = urllib.request.urlopen(url)
     data = json.load(response)["data"]
     
     # save as csv
-    pd.read_json(json.dumps(data)).to_csv(r'C:\\Users\\marco\\OneDrive - Imperial College London\\Trabajo\\Ethereum\\Tokens\\Coinmetrics\\' \
-              + coin + op + '.csv', index = None)
-        
-getUserTransaction('zrx')
-getUserTransaction('zrx', 'accounts')
-getUserTransaction('zrx', 'balance-updates')
-getUserTransaction('zrx', 'blocks')
+    pd.read_json(json.dumps(data)).to_csv(dest + coin + op + '.csv', index = None)
